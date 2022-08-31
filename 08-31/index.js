1568263b7d0fa63e113dc7b1e5cdd4873995cbd4
nateL87 <nathanlyu87@gmail.com>
2022-08-31 12:03:04 +0900

    자료형  연산자 등 배움

--- 08-31/index.js
+++ 08-31/index.js
@@ -1,6 +1,35 @@
 var a = 3;
 let b = 3;
 const c= 3;
-console.log('a :',a);
-console.log('b :',b);
-console.log('c :',c);
+a = 7;
+b = 5;
+c = 4;
+// console.log('a :',a);
+// console.log('b :',b);
+// console.log('c :',c);
+a = 6;
+console.log('a+b=',aPlusB);
+console.log('a-b=', a-b);
+console.log('a*b=', a*b);
+console.log('a/b=', a/b);
+console.log('a%b=', a%b);
+// a = a + 4;
+a += 4;
+a -= 4;
+a *= 4;
+console.log('a-b=')
+
+var e = 3;
+var result = = e--;
+
+var aa = 3;
+var bb = 3;
+console.log('aa  < bb :', aa < bb) ;
+console.log('aa  <= bb :', aa <= bb) ;
+console.log('aa  > bb :', aa < bb) ;
+
+
+
+var f= true;
+var g = false;
+console.log('e:')
